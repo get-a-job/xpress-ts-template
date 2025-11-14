da37a555adae90e5f5f40c8cdd4a7916a84f38d7
grey-shirt <grey-shirt@hotmail.com>
2025-11-14 15:19:19 +0000

    Increase test timeout for testcontainers

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         globals: true,
+        testTimeout: 60_000,
+        hookTimeout: 60_000,
+        teardownTimeout: 60_000,
         environment: "node",
         include: ["src/**/*.test.ts"],
         coverage: {
